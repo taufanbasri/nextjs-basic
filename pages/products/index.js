f144efcc90ac172f32c7b633d7ef85a9eb97681e
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-10 12:27:28 +0700

    Static Site Generation (SSG) + getStaticProps

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,27 @@
-const Products = () => {
+const Products = ({ products }) => {
   return (
     <div>
       <h2>Product List</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default Products;
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:5000/products");
+  const data = await response.json();
+
+  return {
+    props: {
+      products: data,
+    },
+  };
+};
