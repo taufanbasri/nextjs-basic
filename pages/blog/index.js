570c310003a6dd1787c3ba10cc3db1bacc4a4fca
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-10 10:57:18 +0700

    Query Parameters

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,16 @@
-const index = () => {
+import { useRouter } from "next/router";
+
+const BlogIndex = () => {
+  const router = useRouter();
+  const { page, limit } = router.query;
+
   return (
     <div>
       <h2>This is Blog Page</h2>
+      <p>Page: {page}</p>
+      <p>Limit: {limit}</p>
     </div>
   );
 };
 
-export default index;
+export default BlogIndex;
